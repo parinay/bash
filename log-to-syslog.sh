47c1274104250879dbb0a169e094338fc0f582f3
Parinay Kondekar <parinay@gmail.com>
2019-02-08 18:12:13 +0530

    script to demostrate use of syslog for updating logs
    Its good practice use syslogs to log execution scripts.
    syslog takes care of updatng the logs in order it
    happens, w/o overstepping or loosing important log info.

---
+++ log-to-syslog.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+readonly SCRIPT_NAME=$(basename $0)
+
+log() {
+  echo "$@"
+  logger -p user.notice -t $SCRIPT_NAME "$@"
+}
+
+err() {
+  echo "$@" >&2
+  logger -p user.error -t $SCRIPT_NAME "$@"
+}
+
+log "writing to stdout"
+err "writing to stderr"
